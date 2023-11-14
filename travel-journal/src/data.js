5a46a7500cfd3799ad1a80359e55de4edd4b8d41
WAYD0S <rishabhraturi3@gmail.com>
2023-11-14 14:03:37 +1100

    initial commit

---
+++ travel-journal/src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji",
+        country: "Japan",
+        travelPeriod: "12 Jan, 2021 - 24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        coverImg: "mtfuji.png",
+        mapsLink: "https://maps.app.goo.gl/SYETdomKqVRcFozh8"
+    }, 
+    {
+        id: 2,
+        title: "Sydney Opera House",
+        country: "Australia",
+        travelPeriod: "27 May, 2021 - 8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        coverImg: "operahouse.png",
+        mapsLink: "https://maps.app.goo.gl/6C9V37nbazSSgxtW9"
+    }, 
+    {
+        id: 3,
+        title: "Geirangerfjord",
+        country: "Norway",
+        travelPeriod: "01 Oct, 2021 - 18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        coverImg: "geirangerfjord.png",
+        mapsLink: "https://maps.app.goo.gl/47h86UzZCrt88eUE6"
+    }
+]
