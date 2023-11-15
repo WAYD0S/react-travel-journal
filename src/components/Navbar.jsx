3a27043713969742e91206060ffce7e3d8869301
WAYD0S <rishabhraturi3@gmail.com>
2023-11-15 20:54:47 +1100

    tag updates for netlify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav>
-      <img src="../../public/navbar-globe.png" alt="travel journal logo" />
+      <img src="./public/navbar-globe.png" alt="travel journal logo" />
       <h4 className="nav-header">my travel journal.</h4>
     </nav>
   );
